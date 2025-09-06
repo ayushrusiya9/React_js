1afa3c08fe988b00fafbcd83f2e8b8f1c95ebf3e
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-09-06 21:29:45 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App'
-import App from './use_css/Css'
+import App from './App'
+// import App from './use_css/Css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
