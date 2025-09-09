d2f9f4cd2d5953e117bdee07f70dc6140504b63b
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-09-09 20:09:37 +0530

    update

---
+++ src/props/Chlid.jsx
@@ -0,0 +1,13 @@
+import Props from "./Props"
+
+const App = () => {
+    return(
+        <>
+        <Props myname="ayush" age="21" email="ayush@gmail.com"/>
+
+
+        </>
+    )
+}
+
+export default App;
---
+++ src/props/Props.jsx
@@ -0,0 +1,11 @@
+const App = (props) => {
+    return(
+        <>
+        <h1>my name is {props.myname}</h1>
+        <h2>my age is {props.age}</h2>
+        <h3>my email is {props.email}</h3>
+        </>
+    )
+}
+
+export default App;
