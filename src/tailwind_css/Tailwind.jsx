ab21c4384085a613a7ada9ec5bb3afe4b7323f5e
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-09-11 16:29:58 +0530

    update

--- src/tailwind_css/Tailwind.jsx
+++ src/tailwind_css/Tailwind.jsx
@@ -6,10 +6,13 @@ const App = () =>{
         <>
         <h1>ayush</h1>
         <div className="bg-red-500 border border-amber-950 ">My name is ayush</div>
-        <div className="flex justify-between items-center ">
-            <p>helllo</p>
-            <p className="border w-20 p-2">hiii</p>
-            <p>bye</p>
+        <div className="w-full h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 bg-[url(https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=)] bg-no-bg-no-repeat bg-bg-cover">
+            <div className="w-60 h-60 border"></div>
+            <div className="w-60 h-60 border"></div>
+            <div className="w-60 h-60 border"></div>
+            <div className="w-60 h-60 border"></div>
+            <div className="w-60 h-60 border"></div>
+            <div className="w-60 h-60 border"></div>
         </div>
         </>  
     )
