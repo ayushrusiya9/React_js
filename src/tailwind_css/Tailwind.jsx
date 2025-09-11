01c3eab052c8337579863499e069b9903b7d38da
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-09-11 22:45:18 +0530

    update

--- src/tailwind_css/Tailwind.jsx
+++ src/tailwind_css/Tailwind.jsx
@@ -6,15 +6,15 @@ const App = () =>{
         <>
         <h1>ayush</h1>
         <div className="bg-red-500 border border-amber-950 ">My name is ayush</div>
-        <div className="w-full h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 bg-[url(https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=)] bg-no-bg-no-repeat bg-bg-cover">
-            <div className="w-60 h-60 border"></div>
-            <div className="w-60 h-60 border"></div>
-            <div className="w-60 h-60 border"></div>
-            <div className="w-60 h-60 border"></div>
-            <div className="w-60 h-60 border"></div>
-            <div className="w-60 h-60 border"></div>
+        <div className="w-full h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4  content-center justify-items-center bg-[url(https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=)] bg-no-repeat bg-cover ">
+            <div className="w-60 h-60 border bg-amber-400"></div>
+            <div className="w-60 h-60 border bg-amber-400"></div>
+            <div className="w-60 h-60 border bg-amber-400"></div>
+            <div className="w-60 h-60 border bg-amber-400"></div>
+            <div className="w-60 h-60 border bg-amber-400"></div>
+            <div className="w-60 h-60 border bg-amber-400"></div>
         </div>
-        </>  
+        </>
     )
 }
 
