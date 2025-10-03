e997a2df996c8910a7516c37b4f28560581d1f09
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-03 16:29:18 +0530

    update

--- src/rendering/Randering.jsx
+++ src/rendering/Randering.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = () => {
   let [login, setLogin] = useState(false);
   let [dark, setDark] = useState(false);
+  let [meassage,setmaessage] = useState(false);
 
   let ans = login ? <h1>Welcome User</h1> : <h1>Please Login</h1>;
   let ansdark = dark ? <h1>Dark activated</h1> : <h1>Dark mode not activated</h1>;
@@ -30,9 +31,14 @@ const App = () => {
           {ansdark}
           <button onClick={() => setLogin(!login)}>
             {login ? "Logout" : "Login"}
+          </button> <br /> <br />
+          <button onClick={() => setDark(!dark)}>
+            {dark ? "Light Mode" : "Dark Mode"}
           </button>
-          <button onClick={() => setDark(!dark)}>Dark Mode</button>
+
           <h1>render</h1>
+          { meassage && <h1>Hello User</h1> }
+          <button onClick={()=>{setmaessage(!meassage)}}>Show Message</button>
         </div>
     </>
   );
