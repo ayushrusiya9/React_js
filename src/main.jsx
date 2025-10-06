d718051726b6d056294513bec774938ae6265c34
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-06 16:09:36 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -15,11 +15,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <MyContext.Provider>
-  <StrictMode>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
   </MyContext.Provider>
 )
- 
