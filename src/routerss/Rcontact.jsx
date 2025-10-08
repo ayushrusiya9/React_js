726f3cd4f68da33d383651697b98bba7b9dbeb92
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-08 16:22:50 +0530

    update

--- src/routerss/Rcontact.jsx
+++ src/routerss/Rcontact.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom"
 
 const Rcontact  = () =>{
 
+  let Navigate = useNavigate()
 
     return (
       <div>
+        <button onClick={()=>{Navigate('/about')}}>go to about page</button>
         <h1>Rcontact</h1>
       </div>
     )
---
+++ src/usenavigate/UseNavigate.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+const useNavigate = () => {
+    const [count, setCount] = useState(0)
+    
+  return (
+    <>
+        <h1>Use Navigate</h1>
+    </>
+  )
+}
