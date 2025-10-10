0615ec33d988c2087cd08bc76825ac4b33b3c933
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-10-10 16:07:48 +0530

    update

--- src/Axios/CRUD.jsx
+++ src/Axios/CRUD.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import Axios from 'axios'
 import axios from 'axios'
+import { data } from 'react-router-dom'
 const CRUD = () => {
-
+    let [Data, setData] = React.useState([])
     useEffect(()=>{
         axios.get('http://localhost:3000/Hotel').then( (res)=> {
             console.log(res)
             console.log(res.data)
+            setData(res.data)
 
         }).catch((err)=>{
             console.log(err)
@@ -15,6 +17,46 @@ const CRUD = () => {
   return (
     <>
         <h1>Show data........</h1>
+        <div>
+            {
+                Data.map((e)=>(
+                    <h2>{e.name}</h2>
+                ))
+            }
+
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age </th>
+                        <th>City</th>
+                        <th>CheckIN</th>
+                        <th>CheckOut</th>
+                        <th>price</th>
+                        <th>people</th>
+                        <th>total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                        {
+                            Data.map((e)=>(
+                                <tr>
+                                    <td>{e.name}</td>
+                                    <td>{e.age}</td>
+                                    <td>{e.city}</td>
+                                    <td>{e.checkIn}</td>    
+                                    <td>{e.checkOut}</td>
+                                    <td>{e.price}</td>
+                                    <td>{e.people}</td>
+                                    <td>{e.total}</td>
+                                </tr>
+                            ))
+                        }
+                    
+                </tbody>
+            </table>
+        </div>
     </>
   )
 }
